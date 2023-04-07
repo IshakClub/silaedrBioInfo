5cdffb0b6b25b3bd711a066a25711a1700d9ce21
IshakClub <100613307+IshakClub@users.noreply.github.com>
2023-04-07 14:44:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ class Tree:
             return
         node = Tree(False)
         if word[0] not in self.tree:
-            self.tree[word[0]] = (node,)
+            self.tree[word[0]] = node
+            node.make_node(word[1:])
         else:
-            self.tree[word[0]] += (node,)
-        node.make_node(word[1:])
+            self.tree[word[0]].make_node(word[1:])
 
     def find_node(self, prefix):
         if len(prefix) == 0:
@@ -20,24 +20,23 @@ class Tree:
         first_let = prefix[0]
         if first_let not in self.tree:
             return
-        for node in self.tree[first_let]:
-            n_node = node.find_node(prefix[1:])
-            if n_node:
-                return n_node
+        n_node = self.tree[first_let].find_node(prefix[1:])
+        if n_node:
+            return n_node
 
-    def all_words(self):
+    def all_words(self, prefix):
         if self.is_word:
-            return ''
-        for let in self.tree:
-            word = let
-            for node in self.tree[let]:
-                word += node.all_words()
-                return word
-
+            result = [prefix]
+        else:
+            result = []
+            for let in self.tree:
+                child = self.tree[let]
+                result += child.all_words(prefix + let)
+        return result
 
 tree = Tree(False)
 tree.make_node('qwe')
 tree.make_node('qyy')
 tree.make_node('aww')
-print(tree.find_node('qy'))
-print(tree.all_words())
+print(tree.find_node('q'))
+print(tree.all_words(''))
